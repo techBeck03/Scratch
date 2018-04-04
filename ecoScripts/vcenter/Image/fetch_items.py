4080a044d5f1cbf025da6a9f17eff55e66fb52c8
Doron Chosnek <dchosnek@cisco.com>
2018-04-04 14:48:27 -0500

    expanded fetch

--- ecoScripts/vcenter/Image/fetch_items.py
+++ ecoScripts/vcenter/Image/fetch_items.py
@@ -14,28 +14,69 @@ import json
 from pyVim.connect import SmartConnect
 from pyVmomi import vim
 
+import requests.packages.urllib3
+from tetpyclient import RestClient
+
+# Disable warnings
+requests.packages.urllib3.disable_warnings()
+
 VC_HOST = os.environ['VCENTER_HOST']
 VC_USER = os.environ['VCENTER_USER']
 VC_PWD = os.environ['VCENTER_PWD']
 TARGET_ITEM = os.environ['FETCH_TARGET']
 
-# required workaround documented at:
-# https://github.com/vmware/pyvmomi/commit/92c1de5056be7c5390ac2a28eb08ad939a4b7cdd
-context = ssl._create_unverified_context()
+# =============================================================================
+# Functions
+# -----------------------------------------------------------------------------
 
-vc = SmartConnect(host=VC_HOST, user=VC_USER, pwd=VC_PWD, sslContext=context)
-content = vc.RetrieveContent()
+def tet_connect():
+    ''' Connects to Tetration using params stored in environment variables. '''
+    return RestClient(
+            os.environ['TETRATION_ENDPOINT'],
+            api_key=os.environ['TETRATION_API_KEY'],
+            api_secret=os.environ['TETRATION_API_SECRET'],
+            verify=False
+        )
+
+def tet_get_vrfs(exclusions):
+    ''' Returns list of all VRFs (except exclusions) '''
+    restclient = tet_connect()
+    resp = restclient.get('/openapi/v1/vrfs')
+    return [x for x in resp.json() if x['name'] not in exclusions]
+
+def tet_get_scopes():
+    restclient = tet_connect()
+    vrf_list = tet_get_vrfs(exclusions=[])
+    vrf_id = 1
+    for v in vrf_list:
+        if v['name'] == os.environ['TENANT_VRF']:
+            vrf_id = v['vrf_id']
+            break
+    resp = restclient.get('/openapi/v1/app_scopes')
+    return [x['name'] for x in resp.json() if x['vrf_id'] == vrf_id]
+
+
+# =============================================================================
+# Main
+# -----------------------------------------------------------------------------
 
 # set up the structure for the message to be returned to ecohub
 pigeon = {
         "status_code": 400,
         "data": [],
-        "message": "Found {} Datacenters.".format(len(dcenters))
+        "message": ""
     }
 
 # at this time, we only handle one fetch command
 if TARGET_ITEM == 'DATACENTERS':
 
+    # required workaround documented at:
+    # https://github.com/vmware/pyvmomi/commit/92c1de5056be7c5390ac2a28eb08ad939a4b7cdd
+    context = ssl._create_unverified_context()
+
+    vc = SmartConnect(host=VC_HOST, user=VC_USER, pwd=VC_PWD, sslContext=context)
+
+    content = vc.RetrieveContent()
     # A list comprehension of all the root folder's first tier children...
     dcenters = [entity for entity in content.rootFolder.childEntity if hasattr(entity, 'vmFolder')]
 
@@ -47,6 +88,30 @@ if TARGET_ITEM == 'DATACENTERS':
     else:
         pigeon['status_code'] = 400
         pigeon['message'] = 'Could not find any datacenters.'
+
+elif TARGET_ITEM == 'VRFS':
+
+    vrfs = tet_get_vrfs(exclusions=['Unknown', 'Tetration'])
+
+    if len(vrfs) > 0:
+        pigeon['status_code'] = 200
+        pigeon['message'] = "Found {} tenant VRFs.".format(len(vrfs))
+        pigeon['data'] = [{'name': x['name'], 'value': x['name']} for x in vrfs]
+    else:
+        pigeon['status_code'] = 400
+        pigeon['message'] = 'Could not find any tenant VRFs.'
+
+elif TARGET_ITEM == 'SCOPES':
+
+    scopes = tet_get_scopes()
+    if len(scopes) > 0:
+        pigeon['status_code'] = 200
+        pigeon['message'] = "Found {} scopes.".format(len(scopes))
+        pigeon['data'] = [{'name': x, 'value': x} for x in scopes]
+    else:
+        pigeon['status_code'] = 400
+        pigeon['message'] = 'Could not find any scopes.'
+
 
 # if this code is executing, ecohub asked us to fetch something we don't 
 # know how to fetch
